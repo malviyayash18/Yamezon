e1160da24f0a24300b8c371326a65573f139cf5e
priya malviya <malviya.yash18@gmail.com>
2020-10-11 20:55:27 +0530

    .

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -13,6 +13,29 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  imageUrlList = ['../../assets/Advertisement/Offer50.png'
+  , 'https://cdn.pixabay.com/photo/2015/02/24/15/41/dog-647528__340.jpg'
+  , 'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg']
+
+  currentImage = this.imageUrlList[0] ;
+  count = 0;
   
+
+  MoveRight() {
+    // Move Right
+    if (this.count < this.imageUrlList.length - 1) {
+      this.count = this.count + 1;
+      this.currentImage = this.imageUrlList[this.count]
+    }
+  }
+
+  MoveLeft() {
+    // Mode Left
+    if (this.count >= 1) {
+      this.count = this.count - 1;
+      this.currentImage = this.imageUrlList[this.count]
+      console.log(this.count)
+    }    
+  }
 
 }
