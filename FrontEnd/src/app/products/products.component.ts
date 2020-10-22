feb9aae154cc496e0d2f1499c26336fb493f8465
priya malviya <malviya.yash18@gmail.com>
2020-10-22 12:15:15 +0530

    BackEnd

--- FrontEnd/src/app/products/products.component.ts
+++ FrontEnd/src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-products',
@@ -8,13 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
+  db = firebase.firestore();
+
   id: any;
 
   constructor(private route: ActivatedRoute) { 
     this.id = this.route.snapshot.params.id
+    console.log(this.id);
+    this.GetProduct();
    }
 
   ngOnInit(): void {
+  }
+
+  GetProduct() {
+    // this.db.collection(`products/0IHxVFtVA0K9b6NT7hpZ`).get().then((querySnapshot) => {
+    //   querySnapshot.forEach((doc) => {
+    //     console.log(doc.data());
+    //   })
+    // })
   }
 
 }
